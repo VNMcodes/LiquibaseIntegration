b1e8c16a854fd1277083856981a0eefb9a321e96
VNMcodes <jayant.singh.x7@gmail.com>
2025-07-21 23:45:18 +0530

    Includes all the necessary dependencies for liquibase project

---
+++ liquibase-integration/src/main/java/com/vnmcodes/liquibase_integration/LiquibaseIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.vnmcodes.liquibase_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseIntegrationApplication.class, args);
+	}
+
+}
---
+++ liquibase-integration/src/test/java/com/vnmcodes/liquibase_integration/LiquibaseIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnmcodes.liquibase_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
